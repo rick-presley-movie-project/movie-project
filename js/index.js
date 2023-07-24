f24f0fdb7eb425cfb8094880ee6ea39d63feca18
Rick Morales <rick.morales7693@gmail.com>
2023-07-24 16:55:17 -0500

    trying to update code in index.js and html

--- js/index.js
+++ js/index.js
@@ -3,61 +3,84 @@
 const DOMAIN = 'http://localhost:3000';
 
 
+console.log(DOMAIN)
+
 // Get all resources
 const getMovies = async () => {
     const response = await fetch(`${DOMAIN}/movies`);
+    console.log();
     const movies = await response.json();
+    console.log(movies);
     return movies;
 };
 
+// getMovies();
 
 // Get specific resource
-// const getUser = async (id) => {
-//     const response = await fetch(`${DOMAIN}/movies/${id}`);
-//     const movies = await response.json();
-//     return user;
-// };
+const getId = async (id) => {
+    const response = await fetch(`${DOMAIN}/movies/${id}`);
+    console.log(response);
+    const movies = await response.json();
+    console.log(movies);
+    return movies;
+};
+
+// getId();
 
 
 // Create resource
-// const createUser = async (user) => {
-//     const options = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(user)
-//     };
-//     const response = await fetch(`${DOMAIN}/users`, options);
-//     const apiResponse = response.json();
-//     return apiResponse;
-// };
+const createMovie = async (movie) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+    };
+    const response = await fetch(`${DOMAIN}/movies`, options);
+    console.log(response);
+    const apiResponse = response.json();
+    console.log(apiResponse);
+    return apiResponse;
+};
+
+// createMovie();
 
 
 // Edit resource
-// const editUser = async (user) => {
-//     const options = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(user)
-//     };
-//     const response = await fetch(`${DOMAIN}/users/${user.id}`, options);
-//     const apiResponse = response.json();
-//     return apiResponse;
-// };
+const editMovie = async (movie) => {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+    };
+    const response = await fetch(`${DOMAIN}/movies/${movie.id}`, options);
+    console.log(response);
+    const apiResponse = response.json();
+    console.log(apiResponse);
+    return apiResponse;
+};
+
+// editMovie();
 
 
 // Delete resource
-// const deleteUser = async (id) => {
-//     const options = {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     };
-//     const response = await fetch(`${DOMAIN}/users/${id}`, options);
-//     const apiResponse = response.json();
-//     return apiResponse;
-// };
+const deleteMovie = async (id) => {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const response = await fetch(`${DOMAIN}/movies/${id}`, options);
+    console.log(response);
+    const apiResponse = response.json();
+    console.log(apiResponse);
+    return apiResponse;
+};
+
+// deleteMovie();
+
+
