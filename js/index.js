bd8f0368944c4e6ee357eef52e11244a25f4b409
Presley Black <presleyblack24@gmail.com>
2023-07-25 15:16:45 -0500

    update function

--- js/index.js
+++ js/index.js
@@ -83,48 +83,137 @@ const getId = async (id) => {
 })();
 
 
-// Create a Movie
+// // Create a Movie
+// $(document).ready(function() {
+//     // Event listener for form submission
+//     $("#movieForm").submit(function(event) {
+//         event.preventDefault(); // Prevent default form submission
+//
+//         // Get movie data from the form
+//         const movieObject = {
+//             title: $("#title").val(),
+//             genre: $("#genre").val(),
+//             rating: $("#rating").val()
+//         };
+//
+//         // AJAX POST request to send movie data to the server
+//         $.ajax({
+//             url: `${DOMAIN}/movies`, // Replace DOMAIN with your server endpoint
+//             type: "POST",
+//             contentType: "application/json",
+//             data: JSON.stringify(movieObject),
+//             success: function(response) {
+//                 // Handle the successful response from the server
+//                 console.log("Movie added successfully!");
+//                 console.log(response);
+//
+//                 // Render the new movie in the movieList div
+//                 const movieListItem = $("<div>").text(
+//                     `Title: ${response.title}, Genre: ${response.genre}, Rating: ${response.rating}`
+//                 );
+//                 $("#movieList").append(movieListItem);
+//
+//                 // Clear the form fields after successful submission
+//                 $("#title").val("");
+//                 $("#genre").val("");
+//                 $("#rating").val("");
+//             },
+//             error: function(error) {
+//                 // Handle errors, if any
+//                 console.error("Error adding movie:", error);
+//             }
+//         });
+//     });
+// });
 $(document).ready(function() {
-    // Event listener for form submission
-    $("#movieForm").submit(function(event) {
-        event.preventDefault(); // Prevent default form submission
+    function renderMovieList() {
+        $.ajax({
+            url: `${DOMAIN}/movies`,
+            type: "GET",
+            success: function(response) {
+                // Clear the table body before rendering the updated list
+                $("#characters tbody").empty();
 
-        // Get movie data from the form
+                response.forEach(function(movie) {
+                    const movieRow = $("<tr>").append(
+                        $("<td>").text(movie.title),
+                        $("<td>").text(movie.genre),
+                        $("<td>").text(movie.id),
+                        $("<td>").text(movie.rating),
+                        $("<td>").append(
+                            $("<button>")
+                                .text("Delete")
+                                .click(function() {
+                                    deleteMovie(movie.id);
+                                })
+                        ),
+                    );
+
+                    $("#characters tbody").append(movieRow);
+                });
+            },
+            error: function(error) {
+                console.error("Error fetching movies:", error);
+            }
+        });
+    }
+
+    $("#movieForm").submit(function(event) {
+        event.preventDefault();
+
         const movieObject = {
             title: $("#title").val(),
             genre: $("#genre").val(),
             rating: $("#rating").val()
         };
 
-        // AJAX POST request to send movie data to the server
         $.ajax({
-            url: `${DOMAIN}/movies`, // Replace DOMAIN with your server endpoint
+            url: `${DOMAIN}/movies`,
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify(movieObject),
             success: function(response) {
-                // Handle the successful response from the server
                 console.log("Movie added successfully!");
                 console.log(response);
 
-                // Render the new movie in the movieList div
-                const movieListItem = $("<div>").text(
-                    `Title: ${response.title}, Genre: ${response.genre}, Rating: ${response.rating}`
-                );
-                $("#movieList").append(movieListItem);
+                // Fetch and render the updated list of movies after a new movie is added
+                renderMovieList();
 
-                // Clear the form fields after successful submission
                 $("#title").val("");
                 $("#genre").val("");
                 $("#rating").val("");
             },
             error: function(error) {
-                // Handle errors, if any
                 console.error("Error adding movie:", error);
             }
         });
     });
+
+    renderMovieList();
+
+    function deleteMovie(movieId) {
+        $.ajax({
+            url: `${DOMAIN}/movies/${movieId}`, // Replace with the actual endpoint for deleting a movie by ID
+            type: "DELETE",
+            success: function(response) {
+                console.log("Movie deleted successfully!");
+
+                // Fetch and render the updated list of movies after a movie is deleted
+                renderMovieList();
+            },
+            error: function(error) {
+                console.error("Error deleting movie:", error);
+            }
+        });
+    }
 });
+
+
+
+
+
+
+
 
 
 
@@ -185,24 +274,23 @@ const editMovie = async (movie) => {
 
 
 
-// Delete Movie
-const deleteMovie = async (id) => {
-    const options = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    const response = await fetch(`${DOMAIN}/movies/${id}`, options);
-    console.log(response);
-    const apiResponse = response.json();
-    console.log(apiResponse);
-    return apiResponse;
-};
+// // Delete Movie-- no use
+// const deleteMovie = async (id) => {
+//     const options = {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     };
+//     const response = await fetch(`${DOMAIN}/movies/${id}`, options);
+//     console.log(response);
+//     const apiResponse = response.json();
+//     console.log(apiResponse);
+//     return apiResponse;
+// };
 
 // Calling Delete Movie (Don't Use)
 // (async ()=> {
 //    await deleteMovie(4)
 // })()
-
 
